a8e0e5697ed226a547bd6d05974978ecfd8b52ee
WangXiaojiang <jiangw@ruc.edu.cn>
2017-02-27 18:03:39 +0800

    First Commit

---
+++ conf/config.py
@@ -0,0 +1,18 @@
+#coding=utf-8
+import pandas as ps
+
+
+
+
+
+
+
+
+KPI_FILE_NAME = '/home/wj/PycharmProjects/ChinaUnicom/TopCellSearch/meta/20170101-0201KPI.csv'
+LEFT_FILE_NAME = '/home/wj/PycharmProjects/ChinaUnicom/TopCellSearch/meta/left'
+Spark_Home = "/usr/local/spark-2.0.spark-bin-hadoop2.7"
+Check_Point_Path = '/home/wj/PycharmProjects/ChinaUnicom/TopCellSearch/checkpoint'
+CIRCLE = 30
+
+
+
